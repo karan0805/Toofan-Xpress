119a4212227d63736430ec710d67092a61ef6fff
kkhitesh <hkowdiki@gmail.com>
2021-06-05 12:12:47 +0530

    Implemented categories

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
 import './App.css';
-import { useState,useEffect } from 'react';
-import axios from 'axios'
+import { useState } from 'react';
 import NewsCards from './components/NewsCards/NewsCards';
 
 
 function App() {
 
-  //API key
-  const key = "06a6f6c07d1e459ba2eaaaa3598ed490"
-
-  const [businessArticles, setBusinessArticles] = useState([])
-  const [showBusiness, setShowBusiness] = useState(false)
-
-  // const [sportsArticles, setSportsArticles] = useState([])
-  // const [showSports, setShowSports] = useState(false)
-
-  useEffect(()=> {
-    axios.get(`https://newsapi.org/v2/top-headlines?language=en&category=business&apiKey=${key}`)
-    .then(res => {
-      console.log(res)
-      setBusinessArticles(res.data.articles)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  },[])
+  const [showBusiness, setShowBusiness] = useState(true)
+  const [showSports, setShowSports] = useState(true)
+  const [showTechnology, setShowTechnology] = useState(false)
+  const [showScience, setShowScience] = useState(false)
+  const [showHealth, setShowHealth] = useState(false)
+  const [showEntertainment, setShowEntertainment] = useState(false)
 
   return (
     <div>
-      <button onClick={() => setShowBusiness(!showBusiness)} >Business</button><hr></hr>
+      <button onClick={() => setShowBusiness(!showBusiness)} >Business</button>
+      <button onClick={() => setShowSports(!showSports)} >Sports</button>
+      <button onClick={() => setShowTechnology(!showTechnology)} >Technology</button>
+      <button onClick={() => setShowScience(!showScience)} >Science</button>
+      <button onClick={() => setShowHealth(!showHealth)} >Health</button>
+      <button onClick={() => setShowEntertainment(!showEntertainment)} >Entertainment</button>
+
+      <hr></hr>
+
       <div className="container">
-      {showBusiness && <NewsCards articles={businessArticles}/>
-      } 
+      {showBusiness && <NewsCards cat={"business"}/>
+      }
       </div>
+      <div className="container">
+      {showSports && <NewsCards cat={"sports"}/>
+      }
+      </div>
+      <div className="container">
+      {showTechnology && <NewsCards cat={"technology"}/>
+      }
+      </div>
+      <div className="container">
+      {showScience && <NewsCards cat={"science"}/>
+      }
+      </div>
+      <div className="container">
+      {showHealth && <NewsCards cat={"health"}/>
+      }
+      </div>
+      <div className="container">
+      {showEntertainment && <NewsCards cat={"entertainment"}/>
+      }
+      </div>
+
     </div>
   );
 }
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{ useState,useEffect } from 'react'
 import NewsCard from '../NewsCard/NewsCard'
+import axios from 'axios'
 
-const NewsCards = ({articles}) => {
+const NewsCards = ({cat}) => {
+  const [articles, setArticles] = useState([])
+
+  //API key
+  const key = "06a6f6c07d1e459ba2eaaaa3598ed490"
+
+  useEffect(()=> {
+    axios.get(`https://newsapi.org/v2/top-headlines?language=en&category=${cat}&apiKey=${key}`)
+    .then(res => {
+      console.log(res)
+      setArticles(res.data.articles)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
+
   return (
     <>
+      <h1>{cat}</h1>
+      <hr />
       {
         articles.map(article => (
           <NewsCard content={article}/>
